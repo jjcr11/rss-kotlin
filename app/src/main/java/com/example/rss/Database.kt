792fc6701d3c8fefb2ca98df100ea49ab58cfe5f
jjcr11 <jjcr0397@gmail.com>
2022-01-17 10:25:02 -0600

    Unique constraint to SourceEntity added

--- app/src/main/java/com/example/rss/Database.kt
+++ app/src/main/java/com/example/rss/Database.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [SourceEntity::class, FeedEntity::class], version = 7)
+@Database(entities = [SourceEntity::class, FeedEntity::class], version = 8)
 @TypeConverters(DatabaseConverters::class)
 abstract class Database: RoomDatabase() {
     abstract fun dao(): DatabaseDao
--- app/src/main/java/com/example/rss/SourceEntity.kt
+++ app/src/main/java/com/example/rss/SourceEntity.kt
@@ -1,10 +1,11 @@
 package com.example.rss
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 //Data class to be used by SourceAdapter
-@Entity(tableName = "SourceEntity")
+@Entity(tableName = "SourceEntity", indices = [Index(value = ["url"], unique = true)])
 data class SourceEntity(@PrimaryKey(autoGenerate = true) var id: Int = 0,
                         var name: String?,
                         var url: String?)
