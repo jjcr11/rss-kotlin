f23d17292d463ad2de2df028590a81b1af5bfea1
jjcr11 <jjcr0397@gmail.com>
2022-01-27 11:31:34 -0600

    Warning if saved is empty added

--- app/src/main/java/com/example/rss/SavedActivity.kt
+++ app/src/main/java/com/example/rss/SavedActivity.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.allViews
+import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rss.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 import java.io.Serializable
 
 class SavedActivity : AppCompatActivity(), FeedAdapterOnClickListener {
@@ -51,6 +57,19 @@ class SavedActivity : AppCompatActivity(), FeedAdapterOnClickListener {
         t.start()
         t.join()
         feedAdapter.setFeeds(feeds)
+        if(feeds.size == 0) {
+            binding.root.removeAllViews()
+            val view = layoutInflater.inflate(R.layout.empty_saved, null)
+            with(view.findViewById<MaterialToolbar>(R.id.mtb).menu) {
+                getItem(0).isEnabled = false
+                getItem(0).icon = getDrawable(R.drawable.ic_bookmark)
+                getItem(1).isEnabled = false
+                getItem(1).icon = getDrawable(R.drawable.ic_open_in_new_2)
+                getItem(2).isEnabled = false
+                getItem(2).icon = getDrawable(R.drawable.ic_share_2)
+            }
+            binding.root.addView(view)
+        }
     }
 
     override fun onClick(feed: FullFeedEntity, position: Int) {
